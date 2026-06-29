cb6bbf20227eae8cec7f147eb35d04f7212ce243
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2026-06-30 00:51:22 +0300

    if

--- Models/Person.cs
+++ Models/Person.cs
@@ -3,6 +3,6 @@ namespace ViewsExample.Models
     public class Person
     {
         public string? Name { get; set; }
-        public DateTime DateofBirth { get; set; }
+        public DateTime? DateofBirth { get; set; }
     }
 }
